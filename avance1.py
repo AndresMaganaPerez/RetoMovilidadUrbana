426de0243529b098806ab46b616a3f03f7e87f4e
Andrés Magaña Pérez <a01067963@itesm.mx>
2022-11-23 18:14:21 -0600

    Movimiento de agentes uno tras otro sobre matriz

--- avance1.py
+++ avance1.py
@@ -20,41 +20,29 @@ import numpy as np
 
 from mesa import Agent, Model
 from mesa.space import SingleGrid
-from mesa.time import BaseScheduler
+from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 
 # AGENT CLASS
 class Car(Agent):
-    def __init__(self, unique_id, model, x, y, signal):
+    def __init__(self, unique_id, model, x, y):
         super().__init__(unique_id, model)
-        self.actual_position = (x, y)
+        self.pos = (y, x)
         self.velocity = 3
         self.stopped = False
-        self.signal = signal
+        self.signal = None
 
     def step(self):
-        self.model.grid.move_agent(self, (self.actual_position[0] + self.velocity, self.actual_position[1]))
-        
-        # Carro que se detendrá en el carril central
+        print(str(self.unique_id) + ": " + str(self.pos))
+        if not (self.model.grid.out_of_bounds((self.pos[0], self.pos[1] + self.velocity))):
+            if self.model.grid.is_cell_empty((self.pos[0], self.pos[1] + self.velocity)):
+                self.model.grid.move_agent(self, (self.pos[0], self.pos[1] + self.velocity))
 
-        # Checar carriles solo para el carril central
-        if (self.actual_position[1] == 1):
-            self.check_roads()
-
-    def check_roads(self):
-        for neighbor in self.model.grid.iter_neighbors(self.actual_position, moore = True, include_center = False, radius = 5):
-            x, y = neighbor.actual_position
-            # Central Lane in Front of Actual Agent
-            if ((y == self.actual_position[1]) and (x <= self.actual_position[0] + 4) and (neighbor.stopped == True)):
-                # Checar carril superior para cambiar posición
-                if not ((y == self.actual_position[1] - 1) and (self.actual_position[0] + 1 < x >= self.actual_position[0] - 4) and (neighbor.stopped != True)):
-                    # Cambia a carril superior
-                    self.model.grid.move_agent(self, (self.actual_position[0] + 1, 0))
-                # Checar carril inferior para cambiar posición
-                elif not ((y == self.actual_position[1] + 1) and (self.actual_position[0] + 1 < x >= self.actual_position[0] - 4) and (neighbor.stopped != True)):
-                    # Cambia a carril inferior
-                    self.model.grid.move_agent(self, (self.actual_position[0] + 1, 2))
-                
+        # elif not (self.model.grid.out_of_bounds((self.pos[0] + self.velocity - 2, self.pos[1]))):
+        #     self.model.grid.move_agent(self, (self.pos[0] + self.velocity - 2, self.pos[1]))
+        else:
+            self.model.grid.remove_agent(self)
+            self.model.schedule.remove(self)
 
 def get_grid(model):
     grid = np.zeros((model.grid.width, model.grid.height))
@@ -72,32 +60,28 @@ class Road(Model):
     def __init__(self, cars):
         super().__init__()
         self.cars = cars
-        self.placeAgents = cars
+        self.current_id = 0
         self.signal = False
-        self.grid = SingleGrid(500, 3, False)
-        self.schedule = BaseScheduler(self)
+        self.grid = SingleGrid(3, 500, False)
+        self.schedule = RandomActivation(self)
         self.datacollector = DataCollector(model_reporters={"Grid": get_grid})
 
     def step(self):
         y = np.random.choice([0, 1, 2])
-        agent = Car(self.placeAgents, self, 0, y, self.signal)
 
-        for i in range(0, 10, 1):
-            if i == np.random.choice([5,6]):
-                lane = np.random.choice([0,1,2])
+        lane = np.random.choice([0,1,2])
+
+        agent = Car(self.next_id(), self, 0, y)
+        if self.cars > 0:
+            if self.grid.is_cell_empty((y, 0)):
                 self.schedule.add(agent)
-                self.grid.place_agent(agent, (lane, 0))
-                self.placeAgents -=1
-
-            if self.placeAgents == 50:
-                self.signal = True
-            else:
-                self.signal = False
+                self.grid.place_agent(agent, agent.pos)
+                self.cars -= 1
 
         self.datacollector.collect(self)
         self.schedule.step()
 
-agents = 100
+agents = 3
 model = Road(agents)
 MAX_GENERATIONS = 200
 
